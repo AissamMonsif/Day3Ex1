e603e176b9342adab8c50384eb5cdbfb6035d4d8
Ahmed Aissam MONSIF <77149153+AissamMonsif@users.noreply.github.com>
2024-08-12 13:18:24 +0100

    Add files via upload

---
+++ src/main/java/com/example/Discover/MessageProvider.java
@@ -0,0 +1,5 @@
+package com.example.Discover;
+
+public interface MessageProvider {
+    String getMessage();
+}
---
+++ src/main/java/com/example/Discover/StandardErrMessageRenderer.java
@@ -0,0 +1,25 @@
+package com.example.Discover;
+
+public class StandardErrMessageRenderer implements MessageRenderer {
+    private MessageProvider messageProvider;
+
+    @Override
+    public void render() {
+        if (messageProvider == null) {
+            throw new RuntimeException(
+                    "You must set the property messageProvider of class:"
+                            + StandardErrMessageRenderer.class.getName());
+        }
+        System.err.println(messageProvider.getMessage());
+    }
+
+    @Override
+    public void setMessageProvider(MessageProvider provider) {
+        this.messageProvider = provider;
+    }
+
+    @Override
+    public MessageProvider getMessageProvider() {
+        return this.messageProvider;
+    }
+}
